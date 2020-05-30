bd3b6c7228c0b6ea2a06339797954e4c148e3508
cyasmani <yasmanicastaneda@yahoo.com>
2020-05-29 20:29:24 -0700

    Finished 2 JS stretch goals

--- index.js
+++ index.js
@@ -384,11 +384,38 @@ function getHTML(/* Code here */){
 
 /* STRETCH 2: Create a function called `randomize` that takes a data array as an argument and returns a the same array in a randomized order. */
 
-function randomize(/* Code here */){
+function randomize(artists){
 
-    /* Code here */
+  var ctr = artists.length, temp, index;
+
+  while(ctr > 0) {
+
+    index = Math.floor(Math.random() * ctr);
+
+    ctr--;
+
+    temp = artists[ctr];
+    artists[ctr] = artists[index];
+    artists[index] = temp;
+  }
+
+  return artists; 
+
+
+
+    /* Code here */ 
+    
 
   }
 
+  console.log(randomize(artists));
+
 
  /* STRETCH 3: Use advanced array methods (.map, .reduce, .filer) to refactor your MVP code (create an array of all artists born in the 1900s with .filter, for example) */
+
+
+ const filtermostpaintings = artists.filter((artists) => {
+   return artists.paintings >= 100;
+ });
+
+ console.log(filtermostpaintings);
